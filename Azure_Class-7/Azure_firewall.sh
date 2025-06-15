f65b2796e184d9dd1af74e4283d750bb84e9094f
devulmah <devulmah19@gmail.com>
2025-06-15 19:48:25 +0530

    Added Azure_HUB & Firewall_rules_class8 pics .

--- Azure_Class-7/Azure_firewall.sh
+++ Azure_Class-7/Azure_firewall.sh
@@ -40,4 +40,23 @@ az vm create --resource-group ${RG} --name SP1-WINSVR1 --image Win2022Datacenter
 # add to subnets.subnet-1 and subnet-2
 #Create the route same as above one for spoke-2-RG.
 #We can not use same route table for routing firewall traffic on sp-1 and sp-2 if both are different region.
+# by using routing table we are changing the trafic which is going to internet directly to firewall
+# Add routes to subnet means traffic should go to firewall sp1 and sp2
+# We are not able to connect sp-1 vm's and sp-2 vm's directly. only option we have that is through bastion host or just serer.
+# connect through bastion host. sp1 server and sp2 server, type mstsc in bastion server it will trigger rdp.
+#check internet connection of the server by using chrome after connecting through bstion host RDP.
+#in firewall we have 3 types of rules
+    #1) Network firewall rule --->> based on IP addesses and ports we can block. internal communications. like frontend  to back end and backend to database.
+
+    #2) Application firewall rule. -->> which is configured application level ouside. like we can block facebook.
+
+    #3) DNAT rules --->> coenncting to Internet.
+#if you want to allow internet to sp-1 and sp-2 we have to change policies of the firewall.
+# Click on Fire policy which is created at the time of Creating Firewall.
+# Createing Network rules. network collection group -->> rule-group-->> rule collections-->> rules
+# You can network watcher HOP it will route to firewall.
+
+
+
+
 
