b0d134b5d6dc877a213ff9c9d1d11b5c9eabebca
devulmah <devulmah19@gmail.com>
2025-06-20 08:52:29 +0530

    Class_9_latest code

--- Azure_Class-9/NSG-1.sh
+++ Azure_Class-9/NSG-1.sh
@@ -4,6 +4,7 @@
 # Create one new RG
 #Application security groups is nothing but group of vm's based on their roles like web,app,db.
 #Service tags: grouping of public ip's based on their service.
+#We we want to establish a communications between the vm's, The firewall would be use.1
 #cloud-config
 
 
@@ -22,7 +23,7 @@ runcmd:
 - systemtl enable nginx
 - echo "<h1>$(cat /etc/hostname)</h1>" >>/var/www/html/index.nginx-debian.html
 
-RG=AZB48ASG
+RG="NSG_RG"
 echo "Creating Azure Resource Group"
 az group create --location eastus -n ${RG}
  
@@ -32,17 +33,69 @@ az network vnet create -g ${RG} -n ${RG}-vNET1 --address-prefix 10.1.0.0/16 \
 
 az network nsg create -g ${RG} -n ${RG}_NSG1 --location eastus
 
-IMAGE='Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest'
+RG="NSG_RG"
+LOCATION="eastus"
+VNET_NAME="${RG}-vNET1"
+SUBNET_NAME="${RG}-Subnet-1"
+NSG_NAME="${RG}_NSG1"
+ADMIN_USERNAME="devulmah"
+ADMIN_PASSWORD='~!Mahesh@123'  # single quotes prevent Bash expansion
+IMAGE="Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
+CLOUD_INIT="cloud-init.txt"
+STORAGE_SKU="StandardSSD_LRS"
 
-az vm create -g ${RG} -n WEBVM1 --image $IMAGE --vnet-name ${RG}-vNET1 \
---subnet ${RG}-Subnet-1 --admin-username adminsree --admin-password "India@123456" --size Standard_B1s --storage-sku StandardSSD_LRS --nsg ${RG}_NSG1 --custom-data cloud-init.txt
+# Create Web VM
+az vm create \
+  --resource-group $RG \
+  --name WEBVM1 \
+  --image $IMAGE \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_NAME \
+  --admin-username $ADMIN_USERNAME \
+  --authentication-type password \
+  --admin-password "$ADMIN_PASSWORD" \
+  --size Standard_B1s \
+  --storage-sku $STORAGE_SKU \
+  --nsg $NSG_NAME \
+  --custom-data $CLOUD_INIT \
+  --location $LOCATION
 
-az vm create -g ${RG} -n APPVM1 --image $IMAGE --vnet-name ${RG}-vNET1 \
---subnet ${RG}-Subnet-1 --admin-username adminsree --admin-password "India@123456" --size Standard_B1s --storage-sku StandardSSD_LRS --nsg ${RG}_NSG1 --custom-data cloud-init.txt
+# Create App VM
+az vm create \
+  --resource-group $RG \
+  --name APPVM1 \
+  --image $IMAGE \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_NAME \
+  --admin-username $ADMIN_USERNAME \
+  --authentication-type password \
+  --admin-password "$ADMIN_PASSWORD" \
+  --size Standard_B1s \
+  --storage-sku $STORAGE_SKU \
+  --nsg $NSG_NAME \
+  --custom-data $CLOUD_INIT \
+  --location $LOCATION
 
-az vm create -g ${RG} -n DBVM1 --image $IMAGE --vnet-name ${RG}-vNET1 \
---subnet ${RG}-Subnet-1 --admin-username adminsree --admin-password "India@123456" --size Standard_B2s --storage-sku StandardSSD_LRS --nsg ${RG}_NSG1 --custom-data cloud-init.txt
+# Create DB VM
+az vm create \
+  --resource-group $RG \
+  --name DBVM1 \
+  --image $IMAGE \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_NAME \
+  --admin-username $ADMIN_USERNAME \
+  --authentication-type password \
+  --admin-password "$ADMIN_PASSWORD" \
+  --size Standard_B2s \
+  --storage-sku $STORAGE_SKU \
+  --nsg $NSG_NAME \
+  --custom-data $CLOUD_INIT \
+  --location $LOCATION
 
+
+
+#Application security Groups:
+# Arbitarily grouping of vm's ( Selceting vm's and grouping them like app vm's, db vm's).
 az vm create -g ${RG} -n WEBVM2 --image $IMAGE --vnet-name ${RG}-vNET1 \
 --subnet ${RG}-Subnet-1 --admin-username adminsree --admin-password "India@123456" --size Standard_B1s --storage-sku StandardSSD_LRS --nsg ${RG}_NSG1 --asgs WEB_ASG --custom-data cloud-init.txt
 
@@ -64,4 +117,4 @@ sudo systemctl start mysql.service
 sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 bind-address            = *
 sudo systemctl restart mysql.service
-telnet 10.1.1.6 3306
+telnet 10.1.1.6 3306# Variables
