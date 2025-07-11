404a1862fbf8d19fb9c87aec931806f0a622ede4
Devulmah <devulmah19@gmail.com>
2025-07-11 00:14:57 -0700

    Update VPN_connections_VNG.sh

--- Azure_Class-5/VPN_connections_VNG.sh
+++ Azure_Class-5/VPN_connections_VNG.sh
@@ -9,6 +9,8 @@
 #Open cmd type ipconfig /all it will give all the info , dns info.
 #type ncpa.cpl it will promt to network connection page.
 #Go to ipv4 option change dns adress which is own server ip. 192.168.1.100 and give on dns ip address 192.168.0.2.
+#install active directory and telnet client in the aws windows server using server manager console.
+#when install active directory we will get one yelow color pop-up, click on promote this service to domain controller.
 #
 
 
