c5bb0c7236c3b96d5eee2d3048356bd3d20c1de8
Devulmah <devulmah19@gmail.com>
2025-07-10 22:29:36 -0700

    Update VPN_connections_VNG.sh

--- Azure_Class-5/VPN_connections_VNG.sh
+++ Azure_Class-5/VPN_connections_VNG.sh
@@ -3,4 +3,8 @@
 #create one vpc and 2 virtual machines with the ip address range 19.168.0.0/16, vm1-192.168.1.100, vm2-192.168.1.200
 #Create linux server in HUB vnet, windows servers for spoke 1 and spoke 2.
 #Change spoke-1 vnet  DNS server -->>custom DNS with 192.168.1.100. [aws server ip]. same as Spoke-2.
-#
+#JumpServerSubnet
+
+
+
+
